f22ba944c5f496223a11d3a40e6fb262dd45653f
oreze <kunas1@o2.pl>
2022-02-28 21:00:03 +0100

    Create .NET 6 solution

---
+++ src/Zawodowcy.Api/Zawodowcy.Api/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new() { Title = "Zawodowcy.Api", Version = "v1" }); });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zawodowcy.Api v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
